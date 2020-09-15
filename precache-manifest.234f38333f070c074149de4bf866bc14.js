0339e6142062e5ff732fd5ffbcc5fbf03e2c6afc
Fatima Sajadi <s.sfatemeh@gmail.com>
2020-09-15 15:52:52 -0400

    Updates

---
+++ precache-manifest.234f38333f070c074149de4bf866bc14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29647f4c55f62c7e19bef90bb47ec64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53a0fa00a17098767b7b",
+    "url": "/static/css/main.9e4bed3e.chunk.css"
+  },
+  {
+    "revision": "75beea4a2b72fd6d7a7a",
+    "url": "/static/js/2.dd9dfe8d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.dd9dfe8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a0fa00a17098767b7b",
+    "url": "/static/js/main.e02e42d5.chunk.js"
+  },
+  {
+    "revision": "fd83e87ba0d28db1d83c",
+    "url": "/static/js/runtime-main.43f2d560.js"
+  },
+  {
+    "revision": "4441af0dbbae3d4cda5720c70a1d18b1",
+    "url": "/static/media/canada-flag.4441af0d.svg"
+  }
+]);
